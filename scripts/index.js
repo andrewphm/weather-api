8656bffb706c56d79c448a293f364eec9db1b2f9
Andrew Pham <andrew.pham@shopify.com>
2023-09-12 21:44:26 -0400

    Finish challenge 6

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,25 @@
 // ------------------------------------------------------
 // Developer Code
 
-const apiKey = '467355df4c808dd6134a3b64e9ace282';
+const apiKey = 'e1cebecabc86c768ed1b0da7810da38b';
 
 // Functions
-function getWeather(apiKey, zip, callback) {
+async function getWeather(apiKey, zip) {
   // Replace this with your own API key!
-  console.log('something happening?');
   const units = 'imperial';
   const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`;
-  fetch(path)
-    .then((res) => res.json())
-    .then((json) => {
-      callback(json);
-    })
-    .catch((err) => console.log(err.message));
+
+  try {
+    const res = await fetch(path);
+    const json = await res.json();
+    console.log(json);
+    const {
+      main: { temp },
+      weather: [{ description }],
+    } = json;
+
+    return { temp, description };
+  } catch (error) {
+    console.error(error);
+  }
 }
