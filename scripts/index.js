965301db23d277d2104b46db3c38d921c6f84198
Andrew Pham <andrew.pham@shopify.com>
2023-09-13 10:47:08 -0400

    Finish challenge 7

--- scripts/index.js
+++ scripts/index.js
@@ -4,15 +4,39 @@
 const apiKey = 'e1cebecabc86c768ed1b0da7810da38b';
 
 // Functions
-async function getWeather(apiKey, zip) {
+async function getWeather(value, name) {
   // Replace this with your own API key!
   const units = 'imperial';
-  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`;
+  let path = '';
+  let lon = '';
+  let lat = '';
+  if (name === 'coordinates') {
+    value = value.split(', ');
+    lat = value[0];
+    lon = value[1];
+  }
+
+  const baseURL = 'https://api.openweathermap.org/data/2.5/weather';
+  let params = '';
+
+  switch (name) {
+    case 'zip':
+      params = `?zip=${value}&appid=${apiKey}&units=${units}`;
+      break;
+    case 'city':
+      params = `?q=${value}&appid=${apiKey}&units=${units}`;
+      break;
+    case 'coordinates':
+      path = coorPath;
+      break;
+    default:
+      params = `?lat=${lat}&lon=${lon}appid=${apiKey}&units=${units}`;
+      break;
+  }
 
   try {
-    const res = await fetch(path);
+    const res = await fetch(baseURL + params);
     const json = await res.json();
-    console.log(json);
     const {
       main: { temp },
       weather: [{ description }],
